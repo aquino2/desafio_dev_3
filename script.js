906a1a59bfceff55fa085c3e9f454df0f1b6fe49
aquino2 <eduarda.aquino@ufpe.com>
2022-05-27 20:14:00 -0300

    diminuição do codigo

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const button = window.document.querySelector(".button")
 const body = document.querySelector(".body")
 const nome_da_cor = document.getElementById("nome_da_cor")
 
-let aux = 0
 const event1 =(event1) =>{
     body.style.background = '#F2F2F2';
     nome_da_cor.innerHTML = "Background:  #F2F2F2";
@@ -17,21 +16,5 @@ const event3 =(event3) =>{
     body.style.background = '#292929';
     nome_da_cor.innerHTML = "Background:  #292929";
     location.reload();
-    //button.addEventListener('click', (event1));
-}
-let event4 =(event1) =>{
-    body.style.background = '#F2F2F2';
-    nome_da_cor.innerHTML = "Background:  #F2F2F2";
-    button.addEventListener('click', (event5));
-}
-let event5 =(event2) =>{
-    body.style.background = '#C7C7C7';
-    nome_da_cor.innerHTML = "Background:  #C7C7C7";
-    button.addEventListener('click', (event6));
-}
-let event6 =(event3) =>{
-    body.style.background = '#292929';
-    nome_da_cor.innerHTML = "Background:  #292929";
-    button.addEventListener('click', (event1));
 }
 button.addEventListener('click', (event1));

133c9c573611b845d1b845784aadcf1a242b5d19
aquino2 <eduarda.aquino@ufpe.com>
2022-05-27 20:10:06 -0300

    bugfix(bugfix/color_change):foi feito um reload no final do evento 3

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const body = document.querySelector(".body")
 const nome_da_cor = document.getElementById("nome_da_cor")
 
 let aux = 0
-let event1 =(event1) =>{
+const event1 =(event1) =>{
     body.style.background = '#F2F2F2';
     nome_da_cor.innerHTML = "Background:  #F2F2F2";
     button.addEventListener('click', (event2));
 }
-let event2 =(event2) =>{
+const event2 =(event2) =>{
     body.style.background = '#C7C7C7';
     nome_da_cor.innerHTML = "Background:  #C7C7C7";
     button.addEventListener('click', (event3));
 }
-let event3 =(event3) =>{
+const event3 =(event3) =>{
     body.style.background = '#292929';
     nome_da_cor.innerHTML = "Background:  #292929";
-    button.addEventListener('click', (event4));
+    location.reload();
+    //button.addEventListener('click', (event1));
 }
 let event4 =(event1) =>{
     body.style.background = '#F2F2F2';
@@ -33,4 +34,4 @@ let event6 =(event3) =>{
     nome_da_cor.innerHTML = "Background:  #292929";
     button.addEventListener('click', (event1));
 }
-button.addEventListener('click', (event1))
+button.addEventListener('click', (event1));
